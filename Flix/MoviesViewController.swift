e7956d5e2b1e9edd9659f3771871717f5eab7618
Akanksha Kakar <34072193+Akanksha0315@users.noreply.github.com>
2021-02-21 10:16:41 +0530

    Poster Images 1. Downloaded and Installed CocoaPods 2. Connectivity of PosterUrl (Poster images)

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,11 +25,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
-        //let baseUrl =  "https://image.tmdb.org/t/p/w185"
-        //let posterPath = movie["poster_path"] as! String
-        //let posterUrl = URL(string: baseUrl + posterPath)
+        let baseUrl =  "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
         
-        
+        cell.posterView.af_setImage(withURL: posterUrl!)
         return cell
     }
     
