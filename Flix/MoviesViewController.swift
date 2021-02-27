091bbd2835270163e97d9c74cd74a5c044c14e42
Akanksha Kakar <34072193+Akanksha0315@users.noreply.github.com>
2021-02-28 00:23:11 +0530

    1. Created file MovieDetailsViewController 2. Associated new file to Main.storyboard 3. Navigation from MovieCell to MovieDetailsViewController 4. Configuring details for each movie and passing data

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -71,14 +71,21 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        print("Loading the Details Screen")
+        //Find selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        //Pass the selected movie to the Details View Controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+   
 
 }
